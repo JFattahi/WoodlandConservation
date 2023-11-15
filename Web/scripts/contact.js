e1f2321a8c09989515431eb801ad1d8852707dac
bencsci <benl275133@gmail.com>
2023-11-14 20:24:01 -0400

    Revamp pages, added contact us
    Revamp header and homepage,  added dropdown menu. Added Contact Us sections.

---
+++ Web/scripts/contact.js
@@ -0,0 +1,23 @@
+const publicKey = "Pn8eDSTRu9fiVDLQY";
+const serviceID = "service_jelguak";
+const templateID = "template_3ewd6wh";
+
+function sendMessage() {
+  (function () {
+    emailjs.init(publicKey);
+  })();
+
+  var params = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    phone: document.querySelector("#phone").value,
+    message: document.querySelector("#message").value,
+  };
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      alert("Thank you, " + params["name"] + "! Your message has been sent");
+    })
+    .catch();
+}
