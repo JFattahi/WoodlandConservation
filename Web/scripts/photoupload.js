b5f8a6227354b2fa455e6f71132430b5eb6469eb
bencsci <benl275133@gmail.com>
2023-12-06 00:24:32 -0400

    More Comments

--- Web/scripts/photoupload.js
+++ Web/scripts/photoupload.js
@@ -1,33 +1,38 @@
+// Get references to the HTML elements
 const imagesInput = document.getElementById("images");
 const preview = document.querySelector(".preview");
 
+// EmailJS configuration
 const publicKey = "Pn8eDSTRu9fiVDLQY";
 const serviceID = "service_jelguak";
 const templateID = "template_c6y1blp";
 
-// store all the images to be uploaded
+// Array to store images to be uploaded
 let imageArray = [];
 
-// monitor for new images to be uploaded
+// Event listener for changes in the images input field
 imagesInput.addEventListener("change", (event) => {
-  // stores a list of the file/image objectss
+  // Get the list of selected files/images
   const fileList = event.target.files;
 
-  // populate image array
+  // Populate the image array with selected files
   for (let element = 0; element < fileList.length; element++) {
     const item = fileList[element];
-
     imageArray.push(item);
   }
 
+  // Display the selected images in the preview section
   displayImages();
 });
 
+// Function to send a message using EmailJS
 function sendMessage() {
+  // Initialize EmailJS with the public key
   (function () {
     emailjs.init(publicKey);
   })();
 
+  // Get user information from the form
   var params = {
     name: document.querySelector("#name").value,
     email: document.querySelector("#email").value,
@@ -35,42 +40,44 @@ function sendMessage() {
     images: document.querySelector("#images").value,
   };
 
+  // Send an email using EmailJS
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
+      // Redirect the user to the success page after successful email submission
       location.replace("/Web/pages/success/photo_success.html");
     })
     .catch();
 }
 
 /**
- * populate the preview section with the images to be uploaded.
- *
+ * Populate the preview section with the selected images.
  * Author: Agowun Muhammad Altaf (A00448118)
  */
 function displayImages() {
-  // reset the images
+  // Reset the preview section
   preview.innerHTML = null;
 
-  // loop though the images in the image array
+  // Loop through the images in the image array
   imageArray.forEach((img, i) => {
-    // create an img tag
+    // Create an img tag
     let image = document.createElement("img");
-    // set the src of the img tag to that of the image
+
+    // Set the src attribute of the img tag to the URL of the selected image
     image.src = URL.createObjectURL(img);
 
-    // remove the image after clicking on it and confirming
+    // Remove the image after clicking on it and confirming
     image.addEventListener("click", () => {
-      // prompt the user for whether they intended to remove the image
+      // Prompt the user for confirmation before removing the image
 
-      // remove the image from the image array
+      // Remove the image from the image array
       imageArray.splice(i, 1);
 
-      // display the images again after that image was removed
+      // Display the remaining images
       displayImages();
     });
 
-    // add the newly created img tag to the preview section
+    // Add the newly created img tag to the preview section
     preview.appendChild(image);
   });
 }
